5ad045a2b4d754f0c1c74873cb5cdc05bcdb7bfc
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2022-03-29 22:59:05 -0600

    adds simple todo style, todo.js file, index.html

---
+++ todos.js
@@ -0,0 +1,15 @@
+const todos = [
+    {
+      text: `Spring Cleaning - Example Todo Done`,
+      complete: true,
+      priority: 1,
+      id: 0,
+    },
+  
+    {
+      text: `You try add a Todo!`,
+      complete: false,
+      priority: 2,
+      id: 1,
+    },
+  ]
